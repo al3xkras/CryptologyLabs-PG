1b3d92a7c641494c3d30dbe282ce9a0185ae7b4f
al3xkras <alexandr.krasovskiy@gmail.com>
2022-11-29 22:35:54 +0100

    Lab6: implement the Miller-Rabin primality test

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -3,19 +3,59 @@ from random import randint
 def is_prime_sqrt(p):
     pass
 
-def is_prime_miller_rabin(p):
-    pass
-
-def fermat_test(n, k):
-    if n == 2:
-        return True
-    if n % 2 == 0:
+def is_prime_miller_rabin(n, k):
+    """
+    Input: n > 2, an odd integer to be tested for primality;
+       k, a parameter that determines the accuracy of the test
+    Output: composite if n is composite, otherwise probably prime
+    write n − 1 as 2s·d with d odd by factoring powers of 2 from n − 1
+    LOOP: repeat k times:
+       pick a randomly in the range [2, n − 1]
+       x ← a d mod n
+       if x = 1 or x = n − 1 then do next LOOP
+       repeat s − 1 times:
+          x ← x 2 mod n
+          if x = 1 then return composite
+          if x = n − 1 then do next LOOP
+       return composite
+    return probably prime
+    """
+    if not n&1 or n==1:
         return False
+    s=1
+    d=n-1
+    while not d&1:
+        s*=2
+        d//=2
     for i in range(k):
-        a = randint(1, n - 1)
-        if pow(a, n - 1) % n != 1:
-            return False
+        a = randint(2,n-1)
+        x = pow(a,d,n)
+        if x==1 or x==n-1:
+            continue
+        for j in range(s-1):
+            x = pow(x,2,n)
+            if x==1:
+                return False
+            if x==n-1:
+                break
+        if x==n-1:
+            continue
+        return False
     return True
 
+def fermat_test(n, k):
+    pass
+
 if __name__ == '__main__':
-    print(fermat_test(15,10))
+    def test_miller_rabin():
+        assert is_prime_miller_rabin(0, 2) is False
+        assert is_prime_miller_rabin(1, 2) is False
+        assert is_prime_miller_rabin(2, 2) is False
+        assert is_prime_miller_rabin(3, 20) is True
+        assert is_prime_miller_rabin(10, 5) is False
+        assert is_prime_miller_rabin(101, 20) is True
+        assert is_prime_miller_rabin(1001, 30) is False
+        assert is_prime_miller_rabin(4547337172376300111955330758342147474062293202868155909489,30000) is True
+        assert is_prime_miller_rabin(4547337172376300111955330758342147474062293202868155909393, 30000) is False
+    test_miller_rabin()
+

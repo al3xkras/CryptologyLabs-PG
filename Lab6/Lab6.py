b52fd1e168e5ed2b52739efc81d46f44c862f320
al3xkras <alexandr.krasovskiy@gmail.com>
2022-11-30 22:48:07 +0100

    Lab6: implement the Fermat primality test & naive primality test

--- Lab6/Lab6.py
+++ Lab6/Lab6.py
@@ -1,72 +1,99 @@
+from Lab6 import *
 from random import randint
+from numpy import array,inf
+from time import time
+from threading import Thread
+import threading
 
-def is_prime_sqrt(p):
-    pass
+def compareExecutionTime(A, B, reps=1, timelimit=3, **kwargs):
+    t1 = []
+    t2 = []
+    res=None
+    r1=None
+    r2=None
+    def f1(**kwargs):
+        nonlocal r1,t1
+        r1=A(**kwargs)
+        t1.append(time() - time1)
+    def f2(**kwargs):
+        nonlocal r2,t2
+        r2=B(**kwargs)
+        t2.append(time() - time2)
+    for i in range(reps):
+        r1,r2=None,None
+        time1 = time()
+        
+        l=len(t1)
+        
+        p1 = Thread(target = f1, kwargs = kwargs)
+        p1.start()
+        p1.join(timeout = timelimit)
+        
+        if len(t1)==l:
+            t1.append(inf)
+        
+        l=len(t2)
+        time2 = time()
+        p2 = Thread(target = f2, kwargs = kwargs)
+        p2.start()
+        p2.join(timeout = timelimit)
+        if len(t2)==l:
+            t2.append(inf)
+        
+        assert r1 is None or r2 is None or r1==r2
+        if res is None:
+            res=r1
+        r1=None
+        r2=None
+    t11 = array(t1, dtype=float)
+    t21 = array(t2, dtype=float)
+    
+    return [(t11.mean(), t11.std()), (t21.mean(), t21.std()), res]
 
-def is_prime_miller_rabin(n, k):
-    """
-    Input: n > 2, an odd integer to be tested for primality;
-       k, a parameter that determines the accuracy of the test
-    Output: composite if n is composite, otherwise probably prime
-    write n − 1 as 2s·d with d odd by factoring powers of 2 from n − 1
-    LOOP: repeat k times:
-       pick a randomly in the range [2, n − 1]
-       x ← a d mod n
-       if x = 1 or x = n − 1 then do next LOOP
-       repeat s − 1 times:
-          x ← x 2 mod n
-          if x = 1 then return composite
-          if x = n − 1 then do next LOOP
-       return composite
-    return probably prime
-    """
-    if not n&1 or n==1:
-        return False
-    s=1
-    d=n-1
-    while not d&1:
-        s*=2
-        d//=2
-    for i in range(k):
-        a = randint(2,n-1)
-        x = pow(a,d,n)
-        if x==1 or x==n-1:
-            continue
-        for j in range(s-1):
-            x = pow(x,2,n)
-            if x==1:
-                return False
-            if x==n-1:
-                break
-        if x==n-1:
-            continue
-        return False
-    return True
+prime=[
+    3,
+    17,
+    101,
+    1013,
+    10099,
+    103141,
+    1016783,
+    10103099,
+    107707549,
+    10770592813,
+    107705911723,
+    4547337172376300111955330758342147474062293202868155909489
+]
+composite=[
+    2,
+    3*17,
+    556,
+    800001,
+    1007*101,
+    103141*1016783,
+    245709213523*5299871829441939,
+    5059489012128901*1298582838728545,
+    4547337172376300111955330758342147474062293202868155909393
+]
+def execFor(n, expected):
+    A=is_prime_miller_rabin
+    p=n
+    print("n =",p)
+    k=min(10000,max(5,p//1000))
+    print("k =",k)
+    time1,time2,res=compareExecutionTime(A,A,timelimit=3, reps=3,n=p,k=k)
+    
+    print("Mean exec. time of %s:        %.12f" % (A.__name__, time1[0]))
+    print("Std. of the exec. time of %s: %.12f" % (A.__name__, time1[1]))
+    print("Result (is prime):",res)
+    assert res==expected
+    print()
+    print()
 
-def fermat_test(n, k):
-    pass
+    
+if __name__=="__main__":
+    for p in prime:
+        execFor(p, True)
 
-if __name__ == '__main__':
-    def test_miller_rabin():
-        assert is_prime_miller_rabin(0, 2) is False
-        assert is_prime_miller_rabin(1, 2) is False
-        assert is_prime_miller_rabin(2, 2) is False
-        assert is_prime_miller_rabin(3, 20) is True
-        assert is_prime_miller_rabin(10, 5) is False
-        assert is_prime_miller_rabin(101, 20) is True
-        assert is_prime_miller_rabin(1001, 30) is False
-        assert is_prime_miller_rabin(4547337172376300111955330758342147474062293202868155909489,30000) is True
-        assert is_prime_miller_rabin(4547337172376300111955330758342147474062293202868155909393, 30000) is False
-    test_miller_rabin()
-    """
-    Conclusions: the Miller-Rabin test is a probabilistic primality test, that
-        allows to determine if a given number is prime with a high probability.
-        It is much faster in comparison to the non-probabilistic square root primality test,
-        and the accuracy of the test results can be specified and changed.
-        This test excels in the primality analysis of large numbers (e.g. >1e6),
-        because of the reduced execution time and resource consumption,
-        however its results are not definite with a probability of 100%.
-        Nevertheless, the Miller-Rabin test is widely known and used. It is considered a classic
-        primality test.
-    """
-
+    for c in composite:
+        execFor(c, False)

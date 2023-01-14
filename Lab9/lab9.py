9eadc390f6d9ad8ba4b2bb7fbaad18067d5ea533
al3xkras <alexandr.krasovskiy@gmail.com>
2023-01-14 14:49:22 +0100

    Lab9, block cipher: bug fixes (feistel, odd length byte array); edit method next_key

--- Lab9/lab9.py
+++ Lab9/lab9.py
@@ -8,10 +8,6 @@ def binary_op(L:bytes, R:bytes,method):
         out.append(method(L[i],R[i]))
     return bytes(out)
 
-def subs(L:bytes,R:bytes):
-    sub=lambda x,y:(x-y)%256
-    return binary_op(L,R,sub)
-
 def xor(L:bytes, R:bytes):
     _add=lambda x,y: x^y
     return binary_op(L,R,_add)
@@ -20,14 +16,14 @@ def next_key(key:bytes):
     b_arr=bytearray(key)
     key_n=bytearray()
     for b in b_arr:
-        key_n.append((b+1)%256)
+        key_n.append((pow(b,2,300)+10)%256)
     return bytes(key_n)
 
 def F(block:bytes, key:bytes):
     res=bytearray()
     i=0
     for byte in block:
-        res.append(byte^key[i]+i)
+        res.append((byte^key[i]+i+pow(i,5,12))%256)
         i=(i+1)%len(key)
     return bytes(res)
 
@@ -35,11 +31,12 @@ def feistel_cipher(binary:bytes, key:bytes,swap=True):
     _len = len(binary)//2
     L0 = binary[0:_len]
     R0 = binary[_len:2 * _len]
+    remainder=binary[2*_len:]
     L1 = R0
     R0_f = F(R0, key)
     R1 = xor(L0, R0_f)
-    return bytes(bytearray(L1) + bytearray(R1)) if swap else\
-                bytes(bytearray(R1) + bytearray(L1))
+    return bytes(bytearray(L1) + bytearray(R1)+bytearray(remainder)) if swap else\
+                bytes(bytearray(R1) + bytearray(L1)+bytearray(remainder))
 
 def swap_lr(binary:bytes):
     _len = len(binary)
@@ -48,7 +45,7 @@ def swap_lr(binary:bytes):
 
 def block_cipher_binary(binary: bytes, key: bytes, decode=False):
     res=binary
-    count=4
+    count=17
     key_fun=next_key
     key_lst=[key]
     if decode:
@@ -72,14 +69,8 @@ def block_p(image, key: bytes):
     return block_cipher_binary(bytes(image), key)
 
 if __name__ == '__main__':
-    b1=bytes(b"abdfdfdf")
-    b2=bytes(b"9sjrnskd")
-    m=b1[0]+b2[0]
-    print(hex(m),hex(m-b2[0]),hex(b1[0]))
-
-    b1b2=xor(b1, b2)
-    b2s=subs(b1b2,b1)
-    print(b2,b2s)
+    b1=bytes(b"some data to encode")
+    b2=bytes(b"cryptography in python")
 
     b_key= b"09jr20d34d"
     print("data",b1)
@@ -87,3 +78,5 @@ if __name__ == '__main__':
     print("enc",encoded)
     decoded=block_cipher_binary(encoded,b_key,decode=True)
     print("dec",decoded)
+    decoded = block_cipher_binary(encoded, b"04jr22d34d", decode=True)
+    print("dec", decoded)
